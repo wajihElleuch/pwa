e098b6063491e892659615ef4af44d91c819578b
wajih eleuch <wajih.elleuch@phoenix.local>
2020-01-07 17:52:19 +0100

    todo service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import {TodoService} from './services/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'paw';
+  update = false;
+  todos: any;
+
+  constructor(private updates: SwUpdate, private todoService: TodoService) {
+    this.updates.available.subscribe(value => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+
+  ngOnInit(): void {
+    this.todoService.getTodo().subscribe(value => {
+      this.todos = value;
+      console.log(value);
+    });
+  }
 }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor(private  httpClient: HttpClient) {
+  }
+
+  public getTodo() {
+    return this.httpClient.get('https://jsonplaceholder.typicode.com/todos');
+  }
+}
