1c11b418c3558814925cb31058eec9d507730380
wajih eleuch <wajih.elleuch@phoenix.local>
2020-01-08 15:37:17 +0100

    users service1

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -63,9 +63,9 @@ export class UserService {
     allItems.forEach((item: User) => {
       console.log(item);
       this.httpClient.put(`https://app-paw.herokuapp.com/users/edit`, item).subscribe(value => console.log(value));
-      this.db.users.delete(item).then(() => {
-        console.log(`item ${item} sent and deleted locally`);
-      });
+      // this.db.users.delete(item).then(() => {
+      //   console.log(`item ${item} sent and deleted locally`);
+      // });
     });
   }
 
