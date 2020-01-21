4a8ad670f76fc135af8def529640fbc45c453c77
wajih eleuch <wajih.elleuch@phoenix.local>
2020-01-21 16:06:44 +0100

    designe modal

--- src/app/components/list-vessels/services/vessels.service.ts
+++ src/app/components/list-vessels/services/vessels.service.ts
@@ -24,7 +24,7 @@ export class VesselsService {
       if (online) {
         console.log('went online');
         console.log('sending all stored items');
-        // this.sendItemsFromIndexedDb();
+        this.sendItemsFromIndexedDb();
       } else {
         console.log('went offline, storing in indexdb');
       }
@@ -92,33 +92,33 @@ export class VesselsService {
 
   updateVessel(vessel) {
     if (!this.onlineOfflineService.isOnline) {
-
+      this.db.vesselToUpdate.put(vessel);
       this.db.vesselList.put(vessel);
       return from(this.db.vesselList.toArray());
     } else {
-      return this.httpClient.put(`http://app-paw.herokuapp.com/edit/vessel`, {vessel});
+      return this.httpClient.put(`http://app-paw.herokuapp.com/edit/vessel`, vessel);
     }
   }
 
-  // private async sendItemsFromIndexedDb() {
-  //   const allItems: Vessel[] = await this.db.users.toArray();
-  //   // this.httpClient.put(`https://app-paw.herokuapp.com/users/edit`, {
-  //   //   firstName: 'dalidali',
-  //   //   id: 2,
-  //   //   lastName: 'jallouli',
-  //   //   selected: false
-  //   // }).pipe(
-  //   //   retryWhen(errors => errors.pipe(delay(1000), take(10)))
-  //   // ).subscribe(value => console.log(value));
-  //
-  //   allItems.forEach((item: Vessel) => {
-  //     console.log(item);
-  //     this.httpClient.put(`https://app-paw.herokuapp.com/users/edit`, item).pipe(
-  //       retryWhen(errors => errors.pipe(delay(1000), take(10)))
-  //     ).subscribe(value => console.log(value));
-  //     this.db.users.delete(item.id).then(() => {
-  //       console.log(`item ${item} sent and deleted locally`);
-  //     });
-  //   });
-  // }
+  private async sendItemsFromIndexedDb() {
+    const allItems: Vessel[] = await this.db.vesselToUpdate.toArray();
+    // this.httpClient.put(`https://app-paw.herokuapp.com/users/edit`, {
+    //   firstName: 'dalidali',
+    //   id: 2,
+    //   lastName: 'jallouli',
+    //   selected: false
+    // }).pipe(
+    //   retryWhen(errors => errors.pipe(delay(1000), take(10)))
+    // ).subscribe(value => console.log(value));
+
+    allItems.forEach((item: Vessel) => {
+      console.log(item);
+      this.httpClient.put(`http://app-paw.herokuapp.com/edit/vessel`, item).pipe(
+        retryWhen(errors => errors.pipe(delay(1000), take(10)))
+      ).subscribe(value => console.log(value));
+      this.db.vesselToUpdate.delete(item.id).then(() => {
+        console.log(`item ${item} sent and deleted locally`);
+      });
+    });
+  }
 }
